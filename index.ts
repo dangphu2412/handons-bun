83dae412f2aac7d136b5423ad7317182fb14d344
dangphu2412 <dangphu241299@gmail.com>
2025-05-04 10:06:08 +0700

    feat: test api with sql database

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import figlet from "figlet";
+import { sql } from "bun";
 
+// https://bun.sh/docs/api/sql#database-environment-variables
+await sql.connect();
+console.log("Connected!");
 
 const server = Bun.serve({
     port: 3000,
@@ -7,7 +11,19 @@ const server = Bun.serve({
         const body = figlet.textSync("Bun!");
         return new Response(body);
     },
+    routes: {
+        "/api/status": new Response("OK"),
+        // Dynamic routes
+        "/users/:id": async req => {
+            const user = await sql`SELECT * FROM users where users.id = ${req.params.id}`
+
+            if (!user) {
+                return Response.json({ msg: 'not found '});
+            }
+
+            return Response.json(user)
+        },
+    }
 });
 
-console.log('This is interesting')
 console.log(`Listening on http://localhost:${server.port} ...`);
